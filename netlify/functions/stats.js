b16ceaf2258092b9cf04cf72fbd2cb1a0783f82f
SiddharthShyniben <siddharth.muscat@gmail.com>
2022-02-16 14:45:20 +0530

    🚧 Check if files are logged

--- netlify/functions/stats.js
+++ netlify/functions/stats.js
@@ -1,7 +1,11 @@
 const {schedule} = require('@netlify/functions');
+const fs = require('fs');
+
+const data = fs.readFileSync('src/lib/data/user.json');
 
 const handler = async function(event, context) {
 	console.log("Received event:", event)
+	console.log(data);
 
 	return {
 		statusCode: 200,
