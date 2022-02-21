a61ea3db52b5d18124bf1a32d04e48b37fedb976
SiddharthShyniben <siddharth.muscat@gmail.com>
2022-02-21 15:16:47 +0530

    Footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,5 @@
+<footer class='bg-russian-violet-600 text-lg p-10'>
+	<h3>Want your own dashboard like this?</h3>
+	<p><a class='text-blue-400 hover:text-blue-300' target='_blank' href='https://github.com/SiddharthShyniben/hanna'>Hanna</a> makes it
+	easy to get your own Hashnode analytics board in a few clicks.</p>
+</footer>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,10 +2,12 @@
 	import Navbar from '$lib/components/Navbar.svelte';
 	import Profile from '$lib/components/Profile.svelte';
 	import Data from '$lib/components/Data.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 </script>
 
 <div class='bg-russian-violet-900 text-white'>
 	<Navbar />
 	<Profile />
 	<Data />
+	<Footer />
 </div>
