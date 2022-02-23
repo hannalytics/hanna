2d99798a4024d26efa8d4b0df01f27c1e009fc22
SiddharthShyniben <siddharth.muscat@gmail.com>
2022-02-23 08:11:41 +0530

    Change hanna url

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,7 @@
 <footer class='bg-russian-violet-600 text-lg p-10'>
 	<h3>Want your own dashboard like this?</h3>
-	<p><a class='text-blue-400 hover:text-blue-300' target='_blank' href='https://github.com/SiddharthShyniben/hanna'>Hanna</a> makes it
-	easy to get your own Hashnode analytics board in a few clicks.</p>
+	<p>
+        <a class='text-blue-400 hover:text-blue-300' target='_blank' href='https://github.com/hannalytics/hanna'>Hanna</a> makes it
+        easy to get your own Hashnode analytics board in a few clicks.
+    </p>
 </footer>
